03f0e9ed8619b52761ebcf3435b5ecbf0e2d09da
bengilbert6000 <bengilbert6000@428ec822-853d-0410-babd-6388d199fcd1>
2007-10-31 10:48:39 +0000

    main.cpp added second ToyBlocks element.h Element now inserts new elements into static lists (one for each axis) still need to add overlap scanning code screenobject.h AABB now has static list of pairs to be used as list of Collision Pairs

--- core/element.h
+++ core/element.h
@@ -1,14 +1,18 @@
 #ifndef _ELEMENT_H_
 #define _ELEMENT_H_
 
+#include <list>
+#include <iostream>
 #include "screenobject.h"
 
 namespace SweepAndPrune {
-class AABB;
+	class AABB;
 
-class Element{
+	class Element{
 	public:
-		Element(AABB* ptaabb, Vector3 vec) : ptAABB(ptaabb), coordinates(vec) {}
+		Element(AABB* ptaabb, Vector3 vec, bool isMax1) : ptAABB(ptaabb), coordinates(vec), isMax(isMax1) {
+			PushIntoList(this, isMax1);
+		}
 		Element* GetSmallerElementX() const { return smallerElements[0]; }
 		Element* GetSmallerElementY() const { return smallerElements[1]; }
 		Element* GetSmallerElementZ() const { return smallerElements[2]; }
@@ -18,7 +22,7 @@ class Element{
 		float GetX() const { return coordinates.GetX(); }
 		float GetY() const { return coordinates.GetY(); }
 		float GetZ() const { return coordinates.GetZ(); }
-	
+
 		AABB* GetAABB() const { return ptAABB; }
 
 		void SetSmallerElementX( Element* elem )  {  smallerElements[0] = elem ; }
@@ -36,6 +40,95 @@ class Element{
 		Element* smallerElements[3];
 		Element* greaterElements[3];
 		Vector3 coordinates;
+		bool isMax;
+		static std::list<Element*>&  ElementListX() {	
+			static std::list<Element*> ElementListX;	// static list of all Element Objects with respect to the x axis
+			return ElementListX;
+		}
+		static std::list<Element*>&  ElementListY() {	
+			static std::list<Element*> ElementListY;	// static list of all Element Objects with respect to the Y axis
+			return ElementListY;
+		}	
+		static std::list<Element*>&  ElementListZ() {	
+			static std::list<Element*> ElementListZ;	// static list of all Element Objects with respect to the Z axis
+			return ElementListZ;
+		}
+		static void PushIntoList(Element* elem, bool isMax) {
+			std::list<Element*>::iterator it = ElementListX().begin();
+			if(isMax) {
+				for (; it != ElementListX().end(); ) {
+					if( elem->GetX() < (*it)->GetX() ) {
+						ElementListX().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListX().end() ) {
+						ElementListX().insert(it, elem);
+						break;
+					}
+				}
+				it = ElementListY().begin();
+				for (; it != ElementListY().end(); ) {
+					if( elem->GetY() < (*it)->GetY() ) {
+						ElementListY().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListY().end() ) {
+						ElementListY().insert(it, elem);
+						break;
+					}
+				}
+				it = ElementListZ().begin();
+				for (; it != ElementListZ().end(); ) {
+					if( elem->GetZ() < (*it)->GetZ() ) {
+						ElementListZ().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListZ().end() ) {
+						ElementListZ().insert(it, elem);
+						break;
+					}
+				}
+			} else {
+				for (; it != ElementListX().end(); ) {
+					if( elem->GetX() < (*it)->GetX() ) {
+						ElementListX().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListX().end() ) {
+						ElementListX().insert(it, elem);
+						break;
+					}
+				}
+				it = ElementListY().begin();
+				for (; it != ElementListY().end(); ) {
+					if( elem->GetY() < (*it)->GetY() ) {
+						ElementListY().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListY().end() ) {
+						ElementListY().insert(it, elem);
+						break;
+					}
+				}
+				it = ElementListZ().begin();
+				for (; it != ElementListZ().end(); ) {
+					if( elem->GetZ() < (*it)->GetZ() ) {
+						ElementListZ().insert( it , elem); 
+						break;
+					}
+					++it;
+					if( it != ElementListZ().end() ) {
+						ElementListZ().insert(it, elem);
+						break;
+					}
+				}
+			}
+		}
 	};
 
 
