b5ed190b3858cf4a4cf6269181a8e85f9dbb7430
bengilbert6000 <bengilbert6000@428ec822-853d-0410-babd-6388d199fcd1>
2007-11-04 01:17:28 +0000

    done

--- csc3406/csc3406.h
+++ csc3406/csc3406.h
@@ -21,16 +21,16 @@ const float PI = 3.14159265;
 /*** Three dimensional vector class fir floats ***/
 class Vector3{ 
 public: 
-	inline float GetX() const { return x; }
-	inline float GetY() const { return y; }
-	inline float GetZ() const { return z; }
-	inline void SetX(float newX) { x = newX; }
-	inline void SetY(float newY) { y = newY; }
-	inline void SetZ(float newZ) { z = newZ; }
-	inline void set(float dx, float dy, float dz){ x = dx; y = dy; z = dz;} 
-	inline void set(Vector3& v){ x = v.x; y = v.y; z = v.z;}
-	inline void flip(){x = -x; y = -y; z = -z;} // reverse this vector
-	inline void setDiff(Vector3& a, Vector3& b)//set to difference a - b
+	float GetX() const { return x; }
+	float GetY() const { return y; }
+	float GetZ() const { return z; }
+	void SetX(float newX) { x = newX; }
+	void SetY(float newY) { y = newY; }
+	void SetZ(float newZ) { z = newZ; }
+	void set(float dx, float dy, float dz){ x = dx; y = dy; z = dz;} 
+	void set(Vector3& v){ x = v.x; y = v.y; z = v.z;}
+	void flip(){x = -x; y = -y; z = -z;} // reverse this vector
+	void setDiff(Vector3& a, Vector3& b)//set to difference a - b
 	{ x = a.x - b.x; y = a.y - b.y; z = a.z - b.z;}
 	void normalize()//adjust this vector to unit length
 	{		
@@ -46,14 +46,14 @@ public:
 	Vector3(float xx, float yy, float zz){x = xx; y = yy; z = zz;}
 	Vector3(const Vector3& v){x = v.x; y = v.y; z = v.z;}
 	Vector3(){x = y = z = 0;} //default constructor
-	inline Vector3 cross(Vector3 b) //return this cross b
+	Vector3 cross(Vector3 b) //return this cross b
 	{
 		Vector3 c(y*b.z - z*b.y, z*b.x - x*b.z, x*b.y - y*b.x);
 		return c;
 	}
-	inline float dot(Vector3 b) // return this dotted with b
+	float dot(Vector3 b) // return this dotted with b
 	{return x * b.x + y * b.y + z * b.z;}
-	inline double AngleNegY() {
+	double AngleNegY() {
 		double angle = acos(-y) * 180/3.14; // From dot product with negative y axis
 		return angle;
 	}
@@ -61,9 +61,6 @@ public:
 		x *= scalar;
 		y *= scalar;
 		z *= scalar;
-	}
-	inline float getMagnitude() const {
-		return sqrtf(x*x + y*y + z*z);
 	}
 private:
 		float x,y,z;
